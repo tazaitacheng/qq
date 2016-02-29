1e2ad84726f63b0f9e334ae99b853bc8b4da7249
hejian <377933961@qq.com>
2016-02-29 20:38:50 +0800

    qq

---
+++ qq/qq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq/qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 何健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
